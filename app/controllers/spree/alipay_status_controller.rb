e98353fb46ecf9fa1d7894858ca5b56c3dd01cb1
atpking <atpking@gmail.com>
2021-04-21 17:20:44 +0800

    add ali pay url to api

--- app/controllers/spree/alipay_status_controller.rb
+++ app/controllers/spree/alipay_status_controller.rb
@@ -1,47 +1,65 @@
-#inspired by https://github.com/spree-contrib/spree_skrill
+# inspired by https://github.com/spree-contrib/spree_skrill
 module Spree
   class AlipayStatusController < StoreController
-    #fixes Action::Controller::InvalidAuthenticityToken error on alipay_notify
+    # fixes Action::Controller::InvalidAuthenticityToken error on alipay_notify
     skip_before_action :verify_authenticity_token
+
+    def order_buy_url
+      order = retrieve_order params['out_trade_no']
+      if order.payments.unprocessed_payments.last
+        product_names = order.products.pluck(:name)
+        alipay = order.payments.unprocessed_payments.last.try(:payment_method)
+        return head 200 if alipay.nil?
+
+        options = {
+          out_trade_no: order.number,
+          notify_url: url_for(only_path: false, controller: 'alipay_status', action: 'alipay_notify'),
+          return_url: url_for(only_path: false, controller: 'alipay_status', action: 'alipay_done'),
+          body: product_names.join(',').truncate(500), # char 1000
+          #:payment_type => 1,
+          subject: product_names.join(',').truncate(128) # char 256
+        }
+        return render json: { url: alipay.provider.url(order, options) }
+      end
+      head 200
+    end
 
     def alipay_done
       # alipay acount could vary in each store.
       # get order_no from query string -> get payment -> initialize Alipay -> verify ailpay callback
-      order = retrieve_order params["out_trade_no"]
-      alipay_payment = get_alipay_payment( order )
+      order = retrieve_order params['out_trade_no']
+      alipay_payment = get_alipay_payment(order)
 
-      if alipay_payment.payment_method.provider.verify?( request.query_parameters )
+      if alipay_payment.payment_method.provider.verify?(request.query_parameters)
         # 担保交易的交易状态变更顺序依次是:
         #  WAIT_BUYER_PAY→WAIT_SELLER_SEND_GOODS→WAIT_BUYER_CONFIRM_GOODS→TRADE_FINISHED。
         # 即时到账的交易状态变更顺序依次是:
         #  WAIT_BUYER_PAY→TRADE_FINISHED。
-        complete_order( order, request.request_parameters )
+        complete_order(order, request.request_parameters)
         if order.complete?
-          #copy from spree/frontend/checkout_controller
+          # copy from spree/frontend/checkout_controller
           session[:order_id] = nil
           flash.notice = Spree.t(:order_processed_successfully)
           flash['order_completed'] = true
-          redirect_to spree.order_path( order )
+          redirect_to spree.order_path(order)
         else
-          #Strange
+          # Strange
           redirect_to checkout_state_path(order.state)
         end
       else
         redirect_to checkout_state_path(order.state)
       end
-
     end
 
     def alipay_notify
-      order = retrieve_order params["out_trade_no"]
-      alipay_payment = get_alipay_payment( order )
-      if alipay_payment.payment_method.provider.verify?( request.request_parameters )
-        complete_order( order, request.request_parameters )
-        render plain: "success"
+      order = retrieve_order params['out_trade_no']
+      alipay_payment = get_alipay_payment(order)
+      if alipay_payment.payment_method.provider.verify?(request.request_parameters)
+        complete_order(order, request.request_parameters)
+        render plain: 'success'
       else
-        render plain: "fail"
+        render plain: 'fail'
       end
-
     end
 
     private
@@ -50,15 +68,16 @@ module Spree
       @order = Spree::Order.find_by_number!(order_number)
     end
 
-    def get_alipay_payment( order )
-      #use payment instead of unprocessed_payments, order may be completed.
+    def get_alipay_payment(order)
+      # use payment instead of unprocessed_payments, order may be completed.
       order.payments.last
     end
 
-    def complete_order( order, alipay_parameters )
+    def complete_order(order, alipay_parameters)
       unless order.complete?
-        alipay_payment = get_alipay_payment( order )
-        alipay_payment.update_attribute :response_code, "#{alipay_parameters['trade_no']},#{alipay_parameters['trade_status']}"
+        alipay_payment = get_alipay_payment(order)
+        alipay_payment.update_attribute :response_code,
+                                        "#{alipay_parameters['trade_no']},#{alipay_parameters['trade_status']}"
         alipay_payment.complete!
         # it require pending_payments to process_payments!
         order.next
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Spree::Core::Engine.add_routes do
   # Add your extension routes here
   get '/alipay_status/done/' => 'alipay_status#alipay_done', :as => :alipay_done
   post '/alipay_status/notify/' => 'alipay_status#alipay_notify', :as => :alipay_notify
-
+  post '/alipay_status/notify/' => 'alipay_status#alipay_notify', :as => :alipay_notify
+  post '/api/v2/storefront/checkout/order_buy_url' => 'spree/api/v2/storefront/cart#abuy_item_directly'
   #fix issue
   #https://github.com/spree/spree_auth_devise/commit/bab2593f75909feeed3f53b54a63c2edd25f7ba5
   #get '/checkout/registration' => 'checkout#registration', :as => :checkout_registration
@@ -16,6 +17,7 @@ Spree::Core::Engine.add_routes do
         member do
           get 'paypal_refund'
           post 'paypal_refund'
+          get 'order_buy_url'
         end
       end
     end
@@ -23,4 +25,3 @@ Spree::Core::Engine.add_routes do
 
 
 end
-
