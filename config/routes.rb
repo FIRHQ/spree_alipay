7b958d2e0897a316053a66a828bbe898fc87b09a
atpking <atpking@gmail.com>
2021-04-22 11:27:00 +0800

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Spree::Core::Engine.add_routes do
   # Add your extension routes here
   get '/alipay_status/done/' => 'alipay_status#alipay_done', :as => :alipay_done
   post '/alipay_status/notify/' => 'alipay_status#alipay_notify', :as => :alipay_notify
-  post '/api/v2/storefront/checkout/order_buy_url' => 'spree/api/v2/storefront/checkout#order_buy_url'
+  post '/api/v2/storefront/checkout/order_buy_url' => 'api/v2/storefront/checkout#order_buy_url'
   #fix issue
   #https://github.com/spree/spree_auth_devise/commit/bab2593f75909feeed3f53b54a63c2edd25f7ba5
   #get '/checkout/registration' => 'checkout#registration', :as => :checkout_registration
